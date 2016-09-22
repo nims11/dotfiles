f28e0201d9b5b6fc3498bfa87fa8f6731a4b24cd
Nimesh Ghelani <nimeshghelani@gmail.com>
2016-09-22 16:37:21 -0400

    use named pipes

--- .config/panel/lbar.sh
+++ .config/panel/lbar.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+mktempfifo(){
+    tmppipe=$(mktemp -u)
+    mkfifo -m 600 "$tmppipe"
+    echo "$tmppipe"
+}
+BAR_PIPE=$(mktempfifo)
+FEEDBACK_PIPE=$(mktempfifo)
+volhandler(){
+    if [[ $# == 1 ]]; then
+        /usr/bin/pavucontrol &
+    else
+        case $2 in
+            up) amixer -D pulse sset Master 3%+ &>/dev/null
+                ;;
+            down) amixer -D pulse sset Master 3%- &>/dev/null
+                ;;
+            *) return
+                ;;
+        esac
+        echo "redraw"
+    fi
+}
+handler(){
+    case $1 in 
+        calendar)
+            /usr/bin/gsimplecal &
+            ;;
+        volume)
+            volhandler "$@" &
+            ;;
+    esac
+}
+
 wname(){
     xdotool getactivewindow getwindowname
 }
@@ -16,7 +49,18 @@ draw_bar(){
     echo "%{l} $(wname)%{r} $(volume)   $(clock) "
 }
 
-while [[ 1 ]]; do 
+
+while read line; do
+    echo $line >> /tmp/lbar.log
     draw_bar
+done < $BAR_PIPE | lemonbar -b -g x30 -B#333 -f "FontAwesome-10" -f "Inconsolata-10" > $FEEDBACK_PIPE &
+
+while read line; do
+    echo $line >> /tmp/lbar.log
+    handler $line
+done < $FEEDBACK_PIPE > $BAR_PIPE &
+
+while true; do
+    echo redraw
     sleep 0.5
-done | lemonbar -b -g x30 -B#333 -f "FontAwesome-10" -f "Inconsolata-10"
+done > $BAR_PIPE
