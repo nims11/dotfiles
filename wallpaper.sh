aa91cba400b924b381895965fca9e94044e08a05
Nimesh Ghelani <nimeshghelani@gmail.com>
2016-10-05 16:14:42 -0400

    fixed command

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    find ~/wallpapers -maxdepth 1 -type f \( name '*.jpg' -i -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-scale
+    find ~/wallpapers -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-scale
     sleep 15m
 done
