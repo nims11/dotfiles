c2c3564a08fcfcb04fc5d3ab09a839fa154e3794
Nimesh Ghelani <nimeshghelani@gmail.com>
2016-12-06 16:43:11 -0500

    i3lock

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-~/wallpaper.sh &
+(~/wallpaper.sh; i3lock -i /tmp/cur_wallpaper.png) &
 compton &
 artha &
 termite &
 sudo netctl start eduroam &
-python2 ~/.config/panel/lbar.py
+python2 ~/.config/panel/lbar.py &
