44e554cbf89c3263ec080d2e9b71bf62d3e91d8a
Nimesh Ghelani <nimeshghelani@gmail.com>
2016-12-29 00:39:46 -0500

    autostart is back

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,8 @@
+(~/wallpaper.sh; i3lock -i /tmp/cur_wallpaper.png) &
+compton &
+artha &
+termite &
+~/.config/start-internet.sh &
+/usr/lib/notification-daemon-1.0/notification-daemon start &
+python2 ~/.config/panel/lbar.py & 
+mpd &
