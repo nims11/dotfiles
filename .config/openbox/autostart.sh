73a608f2957af1268e0808ca65e5d855c55b69b5
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-01-13 17:43:44 -0500

    add colorscheme.sh

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-xrdb ~/.Xresources &
+~/.config/colorscheme.sh apply &
 (~/wallpaper.sh; i3lock -i /tmp/cur_wallpaper.png) &
 compton &
 artha &
 termite &
 ~/.config/start-internet.sh &
 /usr/lib/notification-daemon-1.0/notification-daemon start &
-python2 ~/.config/panel/lbar.py & 
 mpd &
 udiskie &
