fc2f959400bee749bd2d44ee965fedbd674f91df
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-05-09 15:12:24 -0400

    Update

--- scripts/xpath.py
+++ scripts/xpath.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 from lxml import html as xpathDoc
 import sys
 html = sys.stdin.read()
-doc = xpathDoc.fromstring(html)
+doc = xpathDoc.fromstring(html.encode())
 for rows in doc.xpath(sys.argv[1]):
-    print str(rows)
+    print(str(rows))
