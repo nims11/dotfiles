32efdaf7cfff28bd243a2cdaa86143e6cb4fb012
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-05-15 14:40:29 -0400

    update dpi

--- .config/colorscheme.sh
+++ .config/colorscheme.sh
@@ -13,6 +13,7 @@ if [[ $1 == "apply" ]]; then
     FONT_SIZE=9
     resolution=$(xdpyinfo | grep 'dimensions:' | awk '{print $2}' | cut -d'x' -f2)
     if [ "$resolution" -gt "900" ]; then
+        xrandr --dpi 83x83
         BAR_HEIGHT=35
         FONT_SIZE=12
     fi
