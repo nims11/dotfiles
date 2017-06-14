c7344d2a12c7544cbbfa96fb33487e4a53d4e4ed
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-06-13 23:16:11 -0400

    Disable i3lock on startup

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ~/.config/colorscheme.sh apply &
-(~/wallpaper.sh; i3lock -i /tmp/cur_wallpaper.png) &
 compton -b --config ~/.config/compton.conf
 artha &
 termite &
+~/wallpaper.sh &
 # ~/.config/start-internet.sh &
 /usr/lib/notification-daemon-1.0/notification-daemon start &
 mpd &
