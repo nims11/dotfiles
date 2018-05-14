d748e2417dbb37e514b880ef1e53fc21a652be8c
Nimesh Ghelani <nimeshghelani@gmail.com>
2018-05-14 14:06:16 -0400

    Update bindings

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 - `pacman -S tmux git neovim python-neovim pkg-config make fakeroot powerline-fonts clang gsimplecal alsa-utils python2-psutil psutils gcc jupyter python-pylint i3lock compton mpd ncmpcpp xdotool mpc imagemagick feh pkgfile`
-- `pacaur -S ttf-font-awesome-4 dmenu2 i3lock-color`
+- `pacaur -S ttf-font-awesome-4 dmenu2 i3lock-color pulseaudio-ctl`
 - `pkgfile --update`
 - `git submodule update`
 - move `~/scripts/adj_brightness` and `~/scripts/pacman_update_list` to `/usr/bin/` and add exception in sudoers
--- scripts/j4-dmenu-launcher.sh
+++ scripts/j4-dmenu-launcher.sh
@@ -3,7 +3,5 @@ BG=$(xrdb -query | egrep '^\*.background\s*:\s*' | tr ':' ' ' | awk '{print $2}'
 FG=$(xrdb -query | egrep '^\*.foreground\s*:\s*' | tr ':' ' ' | awk '{print $2}' | tail -1)
 HEIGHT=$(xrdb -query | egrep '^lbar.height\s*:\s*' ~/.Xresources | tr ':' ' ' | awk '{print $2}' | tail -1)
 FONTSIZE=$(xrdb -query | egrep '^lbar.fontsize\s*:\s*' ~/.Xresources | tr ':' ' ' | awk '{print $2}' | tail -1)
-echo $HEIGHT
-echo $FONTSIZE
 dmenu_run -b -q -i -h $HEIGHT -p ">" -fn "Ubuntu Mono - $FONTSIZE" \
     -nb $BG -nf $FG -sb $FG -sf $BG
